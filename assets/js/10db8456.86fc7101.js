7f454914fb7add659c13e86f0a6ffc6906831d33
Michael Dougal <michaeldougal@yahoo.com>
2026-02-20 18:40:42 -0600

    Built and published by Moonwave

---
+++ assets/js/10db8456.86fc7101.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[651],{4410:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/michaeldougal/framework/blob/master","baseUrl":"/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/eea67db2.6f3efe88.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[901],{787:s=>{s.exports=[]}}]);
